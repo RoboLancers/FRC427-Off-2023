88534b57f9a2234b0bbc9aa014224d0bb5b5e1da
taannhauser <8504769@philasd.org>
2023-10-13 17:21:33 -0400

    added stuff  to balance robot on charging pad

--- src/main/java/frc/robot/subsystems/intake/commands/TakeIn.java
+++ src/main/java/frc/robot/subsystems/intake/commands/TakeIn.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.intake.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.intake.Intake;
 
 public class TakeIn extends CommandBase {
--- src/main/java/frc/robot/subsystems/intake/commands/TakeOut.java
+++ src/main/java/frc/robot/subsystems/intake/commands/TakeOut.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems.intake.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.intake.Intake;
 
 public class TakeOut extends CommandBase {
